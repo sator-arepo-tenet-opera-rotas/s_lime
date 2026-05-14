4b169519efd7a3e9cd808a53c25ac05a17829117
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-05-14 00:32:09 -0700

    Update maple.h

--- tests/int8n/maple.h
+++ tests/int8n/maple.h
@@ -1,1 +1,2 @@
 ᐱᓕᕆᐊᖅ ᐱᓚᐅᖅᑐᖅ ᓇᓗᓇᐃᒃᑯᑕᕐᒥᒃ SIGSEGV, ᐊᕕᒃᑐᖅᓯᒪᓂᖏᑦ ᑕᒻᒪᖅᓯᒪᔪᑦ. 0x403245a3 fbBlt-ᒥ (srcᓚᐃᓐ=0xc1a1c180, srcStraid=59742, srcX=0, dstᓚᐃᓐ=0x4240cb6c, dstᔅᑐᕋᐃᓐ=1152, ᐊᒡᒍᖅᑐᖅᓯᒪᔪᖅ=0, ᓯᕕᑐᓂᖓ=32961, 442 pm=1111538028, bpp=32, ᑭᖑᒧᑦ=0, ᖁᒻᒧᐊᑦᑎᑕᐅᓯᒪᔪᖅ=0) ᐅᕙᓂ fbblt.c:174 174 *dst++ = FbDoDestInvariantMergiRop(*src++);
+ᓯᒪ
