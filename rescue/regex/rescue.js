81314954bcbee13562a1a8dfbaa58c419e0d9562
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-06-24 12:40:50 -0700

    Create rescue.js

---
+++ rescue/regex/rescue.js
@@ -0,0 +1,11 @@
+// Character classes
+[xyz], [^xyz], ., \d, \D, \w, \W, \s, \S, \t, \r, \n, \v, \f, [\b], \0, \cX, \xHH, \uHHHH, \u{H…H}, x|y 	
+
+//Assertions
+^, $, \b, \B, x(?=y), x(?!y), (?<=y)x, (?<!y)x 	
+
+// Groups and backreferences
+(x), (?<Name>x), (?:x), \n, \k<Name> 	
+
+// Quantifiers
+x*, x+, x?, x{n}, x{n,}, x{n,m}
